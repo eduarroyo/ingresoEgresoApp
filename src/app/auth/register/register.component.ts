fe9ce8310ebba8852d41d5c53a13570ab4c6f517
ARROYO RAMIREZ Eduardo <eduardo.arroyo@berger-levrault.com>
2024-01-05 16:36:00 +0100

    Form registro

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,9 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html'
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
 
+  public registroForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit() {
+    this.registroForm = this.fb.group({
+      nombre: [ '', Validators.required ],
+      correo: [ '', Validators.required ],
+      password: [ '', Validators.required ]
+    });
+  }
+
+  crearUsuario() {
+    console.log(this.registroForm);
+    console.log(this.registroForm?.valid);
+    console.log(this.registroForm?.value);
+  }
 }
